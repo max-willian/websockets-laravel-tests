02ee7dd8930586a6a6fa60f5dbaac5a5eb60cf3d
Max Willian <maxwillian63@gmail.com>
2019-08-28 14:36:20 -0300

    colocando pusher em prod

--- public/js/app.js
+++ public/js/app.js
@@ -60083,9 +60083,11 @@ window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/d
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'pusher',
   key: "anyKey",
-  wsHost: window.location.hostname,
-  wsPort: 6001,
-  disableStats: true
+  forceTLS: true //only prod
+  // wsHost: window.location.hostname,
+  // wsPort: 6001,
+  // disableStats: true
+
 });
 window.Echo.channel('DemoChannel').listen('WebsocketDemoEvent', function (e) {
   console.log(e);
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -50,9 +50,10 @@ window.Pusher = require('pusher-js');
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
-    wsHost: window.location.hostname,
-    wsPort: 6001,
-    disableStats: true
+    forceTLS: true //only prod
+    // wsHost: window.location.hostname,
+    // wsPort: 6001,
+    // disableStats: true
 });
 
 window.Echo.channel('DemoChannel')
